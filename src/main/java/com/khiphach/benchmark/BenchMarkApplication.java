3ed3f862ef32be9d19e5b7e66bd192afe9a6e98c
thanhlh7 <thanhlh7@fsoft.com.vn>
2019-02-18 14:07:47 +0700

    Update memory cache

--- src/main/java/com/khiphach/benchmark/BenchMarkApplication.java
+++ src/main/java/com/khiphach/benchmark/BenchMarkApplication.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.jsoup.Jsoup;
 import org.springframework.boot.SpringApplication;
@@ -18,39 +20,64 @@ import org.springframework.web.bind.annotation.RestController;
 public class BenchMarkApplication {
 
 	private static final String GOOGLE_BASE_QUERY = "https://www.google.com.vn/search?q=%s passmark";
-	private static final String TAG_H3 = "h3";
 	private static final String TAG_A = "a";
-	private static final String TAG_HREF = "href";
 	private static final String TAG_STYLE = "style";
 	private static final String PASSMARK_BENCHMARK_FIELD = "font-family: Arial, Helvetica, sans-serif;font-size: 35px;	font-weight: bold; color: red;";
+	private static final String AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36";
+	private static final Map<String, Integer> CACHE_GPU = new HashMap<>();
+	private static final Map<String, Integer> CACHE_CPU = new HashMap<>();
 
 	public static void main(String[] args) {
 		SpringApplication.run(BenchMarkApplication.class, args);
 	}
 
 	@GetMapping("/get")
-	public ResponseEntity<Integer> getBenchMark(@RequestParam(required = false) String gpu,
+	public ResponseEntity<Result> getBenchMark(@RequestParam(required = false) String gpu,
 			@RequestParam(required = false) String cpu) {
-		int totalBenchmark = 0;
+		Result result = new Result();
+		getGPUBenchmark(gpu, result);
+		getCPUBenchmark(cpu, result);
+		result.generateTotal();
+		return ResponseEntity.ok(result);
+	}
+
+	private void getGPUBenchmark(String gpu, Result result) {
 		try {
 			if (gpu != null) {
-				totalBenchmark += Integer
-						.valueOf(getPassmarkBenchmark(getFirstResultOfGoogleResult(gpu)));
-			}
-			if (cpu != null) {
-				totalBenchmark += Integer.valueOf(getFirstResultOfGoogleResult(cpu));
+				String gpuLowerCase = gpu.toLowerCase();
+				if (CACHE_GPU.containsKey(gpuLowerCase)) {
+					result.setGpu(CACHE_GPU.get(gpuLowerCase));
+				} else {
+					int tmp = Integer.parseInt(getPassmarkBenchmark(getFirstResultOfGoogleResult(gpu)));
+					result.setGpu(tmp);
+					CACHE_GPU.put(gpuLowerCase, tmp);
+				}
 			}
 		} catch (Exception e) {
-			e.printStackTrace();
-			return ResponseEntity.ok(0);
+			result.setGpu(0);
 		}
-		return ResponseEntity.ok(totalBenchmark);
+	}
+
+	private void getCPUBenchmark(String cpu, Result result) {
+		try {
+			if (cpu != null) {
+				String cpuLowerCase = cpu.toLowerCase();
+				if (CACHE_CPU.containsKey(cpuLowerCase)) {
+					result.setCpu(CACHE_CPU.get(cpuLowerCase));
+				} else {
+					int tmp = Integer.parseInt(getPassmarkBenchmark(getFirstResultOfGoogleResult(cpu)));
+					result.setCpu(tmp);
+					CACHE_CPU.put(cpuLowerCase, tmp);
+				}
+			}
+		} catch (Exception e) {
+			result.setCpu(0);
+		}
 	}
 
 	private String getFirstResultOfGoogleResult(String q) throws IOException {
-		return Jsoup.connect(String.format(GOOGLE_BASE_QUERY, q)).userAgent(
-				"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36")
-				.get().getElementsByAttributeValue("class", "r").get(0).getElementsByTag(TAG_A).get(0).attr("href");
+		return Jsoup.connect(String.format(GOOGLE_BASE_QUERY, q)).userAgent(AGENT).get()
+				.getElementsByAttributeValue("class", "r").get(0).getElementsByTag(TAG_A).get(0).attr("href");
 	}
 
 	@SuppressWarnings("unused")
@@ -60,8 +87,41 @@ public class BenchMarkApplication {
 	}
 
 	private String getPassmarkBenchmark(String link) throws IOException {
-		return Jsoup.connect(link).userAgent(
-				"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36")
-				.get().getElementsByAttributeValue(TAG_STYLE, PASSMARK_BENCHMARK_FIELD).get(0).text();
+		return Jsoup.connect(link).userAgent(AGENT).get()
+				.getElementsByAttributeValue(TAG_STYLE, PASSMARK_BENCHMARK_FIELD).get(0).text();
+	}
+
+	public static class Result {
+		private int gpu;
+		private int cpu;
+		private int total;
+
+		public int getGpu() {
+			return gpu;
+		}
+
+		public void setGpu(int gpu) {
+			this.gpu = gpu;
+		}
+
+		public int getCpu() {
+			return cpu;
+		}
+
+		public void setCpu(int cpu) {
+			this.cpu = cpu;
+		}
+
+		public int getTotal() {
+			return total;
+		}
+
+		public void setTotal(int total) {
+			this.total = total;
+		}
+
+		public void generateTotal() {
+			this.total = this.cpu + this.gpu;
+		}
 	}
 }
