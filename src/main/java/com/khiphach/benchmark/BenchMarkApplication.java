b8a90dc3559d7f712e1b451c3f041280d5fbb154
lehuuthanh5 <huuthanh.qt.spkt@gmail.com>
2019-02-19 11:12:24 +0700

    Update CORS

--- src/main/java/com/khiphach/benchmark/BenchMarkApplication.java
+++ src/main/java/com/khiphach/benchmark/BenchMarkApplication.java
@@ -11,12 +11,14 @@ import org.jsoup.Jsoup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@CrossOrigin
 public class BenchMarkApplication {
 
 	private static final String GOOGLE_BASE_QUERY = "https://www.google.com.vn/search?q=%s passmark";
